03e81260c8501d7bfc7ebfaafc4550cf22b4bf6b
AxelFN <130577655+AxelFN@users.noreply.github.com>
2026-04-06 12:26:10 -0600

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Backend funcionando "}
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://TU-FRONTEND.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
